c5a94eb9f50e60ba3100555de6952252dfb23942
rinchinovaoc <117152947+rinchinovaoc@users.noreply.github.com>
2023-01-26 16:17:28 +0800

    Add files via upload

--- dags/parse_rss.py
+++ dags/parse_rss.py
@@ -1,27 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 from work_psql import save_input_data
+from datetime import datetime
+from work_psql import conn_exec_query
+
+conn_id = 'postgre_conn'
 
 def parse_source():
-    url = 'https://tass.ru/rss/v2.xml'
+    url = 'https://lenta.ru/rss/'
+    # url = 'https://www.vedomosti.ru/rss/news'
+    # url = 'https://tass.ru/rss/v2.xml'
     page = requests.get(url)
-    print(page.status_code)
     allNews = []
     news = BeautifulSoup(page.text, "html.parser")
-    print(news)
     save_input_data(data=news)
-
+    # print(news)
     allNews = news.findAll('item')
     for new in allNews:
-        #if data.find('span', class_='time2 time3') is not None:
-        print(new.title.text)
-        print(new.link)
-        
+        # print(new)
+        # print(new.link.text)
+        # print(new.enclosure.url)
+        # if not new.pubDate is None
+        # new_pubdate = datetime.strptime(, '%a, %d %b %Y %X %z')
+        # print(new_pubdate)
         allCategory = []
         allCategory = new.findAll('category')
         for category in allCategory:
-            print(category.text)
-
-        #filteredNews.append(data.category.text)
-   # for data in filteredNews:
-    #    print(data)
+            try:
+                query = "INSERT INTO public.d_news (source, author, title, pubdate, category) select '"+url+"','"+new.author.text+"', '"+new.title.text+"','"+new.pubdate.text+"','"+new.category.text+"'"
+                conn_exec_query(conn_id = 'postgre_conn', query = query)
+            except:
+                print(Exception,'!!!!!!')
+     
