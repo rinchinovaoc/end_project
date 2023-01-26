6fb841a3b739e6b3d6b84bbdfead6ab27569b411
rinchinovaoc <117152947+rinchinovaoc@users.noreply.github.com>
2023-01-26 16:19:52 +0800

    Delete tasks.py

--- dags/tasks.py
+++
@@ -1,8 +0,0 @@
-from parse_rss import parse_source
-
-from work_psql import conn_exec_query
-
-def conn1():
-    conn_exec_query(conn_id = 'postgre_conn', query = 'SELECT table_name FROM information_schema.tables')
-    #conn_exec_query()
-
